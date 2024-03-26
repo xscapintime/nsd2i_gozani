fd785859276f52c52bfec94de335c265a5fde2b9
xscapintime <louiesxscape@gmail.com>
2024-03-26 01:09:57 -0400

    k36me2 at open chromatin clustering

--- mark_profile/signal_on_peak/regions_cluster.R
+++ mark_profile/signal_on_peak/regions_cluster.R
@@ -16,25 +16,54 @@ densityHeatmap(mat, title = " ", ylab= "K36me2 log2(NSD2i/DMSO)")
 dev.off()
 
 
-htcol <- colorRamp2(c(min(mat), 0, max(mat)),  c("#2E5A87", "white", "#A90C38"))
+# htcol <- colorRamp2(c(fivenum(mat)[1:4], 0, max(mat)/2, max(mat)), c(rev(brewer.pal(5, "Blues"))[-5],"#FFFFFF", brewer.pal(3, "Reds")[-1]))
+
+## annotation
+# peak id
+instc_id <- read.csv("../../niceseq_profile/signal_on_peak/intersect_all.bed", header = F, sep = "\t")
+
+# promoter and enhancer
+promoters <- read.csv("../../niceseq/peaks/peak_breakdown/cage_intersect_all.bed", sep = "\t", header = F)[,1:3] 
+enhancers <- read.csv("../../niceseq/peaks/peak_breakdown/enhancer_intersect_all.bed", sep = "\t", header = F)[,1:3]
 
 
-ht1 <- Heatmap(mat, name = "open chromatin", row_km = 3, cluster_columns = F, show_row_names=F,
-clustering_distance_rows = "spearman", col = htcol)
-pdf("heatmap.pdf", width = 3, height = 6)
-draw(ht1)
+promoters <- merge(instc_id, promoters, by.x = c("V1", "V2", "V3"), by.y = c("V1", "V2", "V3")) 
+enhancers <- merge(instc_id, enhancers, by.x = c("V1", "V2", "V3"), by.y = c("V1", "V2", "V3"))
+
+
+## annot heatmap
+ann <- data.frame(row.names(mat), 
+ifelse(row.names(mat) %in% promoters$V4, "promoter", 
+    ifelse(row.names(mat) %in% enhancers$V4, "enhancer", "other")))
+
+colnames(ann) <- c("peakid", "type")
+ann$color <- ifelse(ann$type == "promoter", "#c9ddd7", ifelse(ann$type == "enhancer", "#D95F02", "lightgrey"))
+
+annocol <- ann$color
+names(annocol) <- ann$type
+
+ha <- rowAnnotation(`Types` = ann$type,
+                    border = F,
+                    width = unit(.5,"mm"),
+                    col = list(`Types` = annocol))
+
+
+htcol2 <- colorRamp2(c(-4,-1, 0.5), c("#2166AC", "#FFFFFF", "#B2182B"))
+
+ht2 <- Heatmap(mat, name = "log2(NSD2i/DMSO)", row_km = 4, cluster_columns = F, show_row_names=F,
+                clustering_distance_rows = "pearson", col = htcol2,
+                right_annotation = ha)
+pdf("heatmap_v2.pdf", width = 5, height = 6)
+draw(ht2)
 dev.off()
 
 
+## enhancer only
+enhancermat <- mat[row.names(mat) %in% enhancers$V4,]
 
-pdf("veh_heatmap.pdf", width = 3, height = 6)
-ht2 <- Heatmap(veh, name = "open chromatin", row_km = 3, cluster_columns = F)
+
+ht3 <- Heatmap(mat, name = "log2(NSD2i/DMSO)", row_km = 4, cluster_columns = F, show_row_names=F,
+                clustering_distance_rows = "pearson", col = htcol2)
+pdf("heatmap_enhancer.pdf", width = 5, height = 6)
+draw(ht3)
 dev.off()
-
-
-
-## extract peak id
-row.names(trt) <- peakid
-nsd2i_openup_peaks <- row.names(trt[row_order(ht1)[[3]],])
-
-write.table(nsd2i_openup_peaks, "nsd2i_openup_peaks.txt", quote=F, col.names=F, row.names=F)
---
+++ niceseq_profile/signal_on_peak/get_data.sh
@@ -0,0 +1,2 @@
+# with peak id
+rsync -avr 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/niceseq_profile/signal_on_peak/intersect_all.bed' .
