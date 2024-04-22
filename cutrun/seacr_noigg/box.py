c1c264fa1a4347ffd0d99481ecbe32c405c91a65
xscapintime <louiesxscape@gmail.com>
2024-04-22 17:20:34 -0400

    average peak length pointplot

--- cutrun/seacr_noigg/box.py
+++ cutrun/seacr_noigg/box.py
@@ -4,7 +4,7 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 
 
-## load
+## peak number
 stats = pd.read_csv('cutnrun.seacr_noigg.stats.txt', sep='\t', header=None)
 stats = stats[:-1]
 stats['mark'] = stats[1].str.split('_').str[3]
@@ -18,7 +18,7 @@ for m in stats['mark'].unique():
 
     g = sns.catplot(data=dat, x='drp', y='num', hue='day', col='trt', kind='bar',\
                     legend=False, height=5, aspect=.6)
-    # g.set(title=m)
+    g.set(title=m)
     g.fig.suptitle(m)
     g.set_axis_labels("", "Peak number")
     for axes in g.axes.flat:
@@ -28,6 +28,36 @@ for m in stats['mark'].unique():
     plt.savefig(f'{m}_seacr_peaknum.pdf')
     plt.close()
 
+    # pointplot
+    plt.figure(figsize=(4,5))
+    g = sns.pointplot(data=dat, x="day", y="num", hue="trt", dodge=True,
+                  palette=["#5773CC","#8B8989" ], linestyles=["-", "--"])
+    g.get_legend().set_title("")
+    g.set_title(m)
+    g.set(xlabel="", ylabel="Peak number")
+    plt.tight_layout()
+    plt.savefig(f'{m}_seacr_pknum_point.pdf')
+    plt.close()
 
 
+## peak average length
+stats = pd.read_csv('cutnrun.seacr_noigg.peaklen.txt', sep='\t', header=0)
+stats['mark'] = stats['sample'].str.split('_').str[3]
+stats['day'] = stats['sample'].str.split('_').str[1]
+stats['trt'] = stats['sample'].str.split('_').str[0]
+
+
+for m in stats['mark'].unique():
+    dat = stats.loc[stats['mark']==m, ] 
+
+    # pointplot
+    plt.figure(figsize=(4,5))
+    g = sns.pointplot(data=dat, x="day", y="ave_peaklen", hue="trt", dodge=True,
+                  palette=["#5773CC","#8B8989" ], linestyles=["-", "--"])
+    g.get_legend().set_title("")
+    g.set_title(m)
+    g.set(xlabel="", ylabel="Ave. peak length")
+    plt.tight_layout()
+    plt.savefig(f'{m}_seacr_pklen_point.pdf')
+    plt.close()
 
--- cutrun/seacr_noigg/get_data.sh
+++ cutrun/seacr_noigg/get_data.sh
@@ -1,3 +1,5 @@
 rsync -avr 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/cutrun/seacr_noigg/cutnrun.seacr_noigg.stats.txt' .
+rsync -avr 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/cutrun/seacr_noigg/cutnrun.seacr_noigg.peaklen.txt' .
+
 
 
