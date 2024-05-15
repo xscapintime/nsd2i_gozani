b42ea18c76c1a1a9d5a68306caca72300344e298
xscapintime <louiesxscape@gmail.com>
2024-05-15 18:12:35 -0400

    attempts for motif

--- niceseq_profile/homer_motif/get_data.sh
+++ niceseq_profile/homer_motif/get_data.sh
@@ -1,1 +1,2 @@
 rsync -av --include="*/" --include="*Results.html" --exclude="*" --exclude='*/' 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/niceseq_profile/homer_motif/' .
+rsync -av --include="*/" --include="knownResults.txt" --exclude="*" --exclude='*/' 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/niceseq_profile/homer_motif/' .
