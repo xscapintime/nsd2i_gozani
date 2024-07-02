1f583ece7474f160fdbcc107c8c3ee23ab3c15fc
xscapintime <louiesxscape@gmail.com>
2024-07-02 14:37:40 -0400

    update ignore

--- niceseq_profile/set_enhancer/deep_profile/plotPrf.sh
+++ niceseq_profile/set_enhancer/deep_profile/plotPrf.sh
@@ -10,40 +10,40 @@ do
 
 
 	if [[ $bn =~ "E2F" ]];then
-                ymax=0.076
-                ymin=0.038
+                ymax=0.091
+                ymin=0.04
 
 	elif [[ $bn =~ "EPITHELIAL" ]];then
-                ymax=0.063
-                ymin=0.032
+                ymax=0.075
+                ymin=0.035
 
         elif [[ $bn =~ "HEDGEHOG" ]];then
-                ymax=0.061
-                ymin=0.03
+                ymax=0.067
+                ymin=0.031
 
 	elif [[ $bn =~ "INTERFERON" ]];then
-                ymax=0.062
-                ymin=0.035
+                ymax=0.071
+                ymin=0.04
 
         elif [[ $bn =~ "KRAS_SIGNALING_DN" ]];then
-                ymax=0.058
-                ymin=0.035
+                ymax=0.071
+                ymin=0.04
 
         elif [[ $bn =~ "KRAS_SIGNALING_UP" ]];then
-                ymax=0.06
+                ymax=0.07
                 ymin=0.032
 
 	elif [[ $bn =~ "MYC_V1" ]];then
-                ymax=0.082
-                ymin=0.04
+                ymax=0.11
+                ymin=0.046
 
 	elif [[ $bn =~ "MYC_V2" ]];then
-                ymax=0.11
-                ymin=0.04
+                ymax=0.12
+                ymin=0.041
 
      	elif [[ $bn =~ "PRC" ]];then
-                ymax=0.054
-                ymin=0.032
+                ymax=0.063
+                ymin=0.036
 
 
 	fi
@@ -51,7 +51,7 @@ do
 	echo $bn $ymax $ymin
 
 	#plotHeatmap -m $f -o $bn.png --colorMap Blues --refPointLabel 'Promoter' --yMin $ymin --yMax $ymax --ymin $ymin --regionsLabel $regions #--colorList "#9F79EE,#8B8989"
-	plotProfile -m $f -o $bn.prof.png --refPointLabel 'Enhancer' --regionsLabel "Gene set" "Control" --colors "#9F79EE" "#8B8989" --plotWidth 5.5 --plotTitle $regions --yMin $ymin --yMax $ymax
+	plotProfile -m $f -o $bn.prof.pdf --refPointLabel 'Enhancer' --regionsLabel "Gene set" "Control" --colors "#9F79EE" "#8B8989" --plotWidth 5.5 --plotTitle $regions --yMin $ymin --yMax $ymax
 
 
 
--- niceseq_profile/set_promoter/deep_profile/get_data.sh
+++ niceseq_profile/set_promoter/deep_profile/get_data.sh
@@ -1,1 +1,1 @@
-rsync -arv 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/niceseq_profile/set_promoter/deep_profile/*.gz' .
+rsync -arv 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/niceseq_profile/set_promoter/deep_profile/*3k.gz' .
