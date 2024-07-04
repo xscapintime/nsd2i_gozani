cf94e31c37b3e3cd4c812ac4a5e17269aefe2048
xscapintime <louiesxscape@gmail.com>
2024-07-04 17:35:39 -0400

    redo ctrl genes selection

--- rnaseq/tpm/get_data.sh
+++ rnaseq/tpm/get_data.sh
@@ -1,1 +1,3 @@
 rsync -avr 'lyshi@narval.alliancecan.ca:/home/lyshi/projects/rrg-majewski-ab/shareroot/projects/collaborators/gozani/RNAseq/checkout/basic/STAR_hg38/2023-12-10_NSD2i_bis/tpm/tximport-tpm.csv' . 
+
+rsync -avr 'lyshi@narval.alliancecan.ca:/home/lyshi/projects/rrg-majewski-ab/shareroot/projects/collaborators/gozani/RNAseq/checkout/basic/STAR_hg38/2023-12-10_NSD2i_bis/featureCounts/annotated_combined.counts' .
