7d04baf0404381a22f6abc7d284e29f9999015f5
xscapintime <louiesxscape@gmail.com>
2024-07-17 17:35:49 -0400

    k27me3 aroung tss

---
+++ mark_profile/set_tss_updown10kb/deep_profile/get_data.sh
@@ -0,0 +1,1 @@
+rsync -avr 'lyshi@narval.alliancecan.ca:/home/lyshi/scratch/nsd2i_gozani_2402/mark_profile/set_tss_updown10kb/deep_profile/*.gz' .
---
+++ mark_profile/set_tss_updown10kb/deep_profile/plotPrf.sh
@@ -0,0 +1,26 @@
+for f in *.gz
+do
+
+	bn=`basename $f | sed "s/.gz//g"`
+	echo $bn
+	
+	regions=`gunzip -c $f | head -n 1 | cut -d "[" -f 9 |  cut -d "]" -f 1 | sed 's/"//g' | sed "s/,/ /g" | sed "s/.bed//g" | sed "s/_prom_1k//g" | cut -d " " -f 1`
+
+	if [[ $bn =~ "d5"  ]];then
+
+		samples=`gunzip -c $f | head -n 1 | cut -d "[" -f 11 | cut -d "]" -f 1 | sed 's/"//g' |  sed "s/,/ /g" | sed "s/_merged//g" | sed "s/.ms_cpm//g"`
+
+	else
+		samples=`gunzip -c $f | head -n 1 | cut -d "[" -f 11 | cut -d "]" -f 1 | sed 's/"//g' |  sed "s/,/ /g" | sed "s/.MSnorm//g" | sed "s/_H/.H/g"`
+
+	fi
+
+
+	plotProfile -m $f -o $bn.prof.pdf --refPointLabel 'TSS' \
+	--regionsLabel "Gene set" "Controls" \
+	--colors "#4682B4" "#CDC9C9" \
+	--plotWidth 7 --plotTitle $regions \
+	--samplesLabel $samples
+	#--yMin $ymin --yMax $ymax
+
+done
